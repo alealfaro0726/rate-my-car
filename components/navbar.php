2e36c71ee355bf3a67f80935324131d1a7cfeee8
Alejandro Alfaro <alealfaro0726@gmail.com>
2022-01-29 13:18:20 -0500

    FIRST

---
+++ components/navbar.php
@@ -0,0 +1,25 @@
+<?php
+    include_once '../includes/connect.php.';
+
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="nav-container">
+        <div class="navbar">
+            <ul>
+                
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
---
+++ includes/connect.php
@@ -0,0 +1,11 @@
+<?php
+    $dbUser = "localhost";
+    $dbRoot = "root";
+    $dbPass = "";
+    $db = "";
+
+    
+    $conn = mysqli_connect($dbUser, $dbRoot, $dbPass, $db);
+
+
+?>
